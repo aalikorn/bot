312f27dc892a093bc1bc034b94ae8014ed531653
Daria Nikolaeva <nikolaevdaaa@gmail.com>
2025-01-27 12:31:08 +0300

    product sevice

--- cmd/bot/main.go
+++ cmd/bot/main.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"os"
 
+	"github.com/aalikorn/bot/internal/service/product"
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 	"github.com/joho/godotenv"
 )
@@ -26,20 +27,40 @@ func main() {
 
 	updates := bot.GetUpdatesChan(u)
 
+	productService = product.NewService()
+
 	for update := range updates {
-		if update.Message != nil {
+		if update.Message == nil {
+			continue
+		}
 
-			if update.Message.Command() == "help" {
-				msg := tgbotapi.NewMessage(update.Message.Chat.ID, "command help")
-				bot.Send(msg)
-				continue
-			}
-			log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
-
-			msg := tgbotapi.NewMessage(update.Message.Chat.ID, "You wrote: "+update.Message.Text)
-			// msg.ReplyToMessageID = update.Message.MessageID
-
-			bot.Send(msg)
+		switch update.Message.Command() {
+		case "help":
+			helpCommand(bot, update.Message)
+		case "list":
+			listCommand(bot, update.Message)
+		default:
+			defaultBehavior(bot, update.Message)
 		}
 	}
+}
+
+func helpCommand(bot *tgbotapi.BotAPI, inputMessage *tgbotapi.Message) {
+	msg := tgbotapi.NewMessage(inputMessage.Chat.ID,
+		"/help - help\n/list - list")
+	bot.Send(msg)
+}
+
+func listCommand(bot *tgbotapi.BotAPI, inputMessage *tgbotapi.Message) {
+	msg := tgbotapi.NewMessage(inputMessage.Chat.ID,
+		"TBD")
+	bot.Send(msg)
+}
+
+func defaultBehavior(bot *tgbotapi.BotAPI, inputMessage *tgbotapi.Message) {
+	log.Printf("[%s] %s", inputMessage.From.UserName, inputMessage.Text)
+	msg := tgbotapi.NewMessage(inputMessage.Chat.ID, "You wrote: "+inputMessage.Text)
+	// msg.ReplyToMessageID = update.Message.MessageID
+
+	bot.Send(msg)
 }
